7102371d7abe718faf945fb0518fa4bc8c1c5817
dhanunjaykumar <mdhanunjay023@gmail.com>
2024-01-18 13:14:52 +0530

    Project setup

---
+++ SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Weather
+//
+//  Created by Dhanunjay Kumar on 14/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, worls!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
